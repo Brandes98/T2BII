e3656d5b9234118d4dddda4ab80a829d154eea08
mauricio <mauriciocamposc1@gmail.com>
2024-04-23 21:11:14 -0600

    On branch develop  Changes to be committed: 	modified:   02 - MongoDB/app.py 	deleted:    02 - MongoDB/create_data.py 	deleted:    02 - MongoDB/data.jsonl 	modified:   02 - MongoDB/docker-compose.yml 	deleted:    02 - MongoDB/prueba.ipynb 	deleted:    02 - MongoDB/quiz2_data.jsonl 	deleted:    02 - MongoDB/testMongo.ipynb 	deleted:    02 - MongoDB/test_mongo.ipynb

--- 02 - MongoDB/app.py
+++
@@ -1,7 +0,0 @@
-from pymongo import MongoClient
-from flask import Flask, request, jsonify
-client = MongoClient('localhost', 27017, username='root', password='example')
-
-db = client['posts']
-posts_collection = db['posts']
-posts_collection.drop()
